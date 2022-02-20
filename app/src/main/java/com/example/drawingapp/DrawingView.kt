fe834c420d04981ec024ef6944a15feddaaf5e6b
yasyas <yasy@users.noreply.github.com>
2022-02-20 13:48:57 +0300

    finished color at the screen

--- app/src/main/java/com/example/drawingapp/DrawingView.kt
+++ app/src/main/java/com/example/drawingapp/DrawingView.kt
@@ -3,6 +3,7 @@ package com.example.drawingapp
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 
@@ -19,6 +20,7 @@ class DrawingView(context: Context,attrs: AttributeSet):View(context, attrs) {
 
     //background , the white background that i want to draw on
     private var canvas: Canvas? = null
+    private var mPaths = ArrayList<CustomPath>()
 
     init {
         setUpDrawing()
@@ -32,7 +34,7 @@ class DrawingView(context: Context,attrs: AttributeSet):View(context, attrs) {
         mDrawPaint!!.strokeJoin = Paint.Join.ROUND
         mDrawPaint!!.strokeCap = Paint.Cap.ROUND
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
-        mBrushSize = 20.toFloat()
+       // mBrushSize = 20.toFloat()//  Move to Main Activity
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
@@ -83,9 +85,13 @@ class DrawingView(context: Context,attrs: AttributeSet):View(context, attrs) {
             else -> return false
         }
         invalidate()
-
        return true
-
+    }
+    fun setSizeForBrush(newSize:Float){
+        mBrushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+        newSize,resources.displayMetrics
+        )
+        mDrawPaint!!.strokeWidth = mBrushSize
     }
 
    internal inner class CustomPath(var color:Int, var brushThickness:Float) : Path() {
--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -1,14 +1,49 @@
 package com.example.drawingapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import kotlinx.android.synthetic.main.dialog_brush_size.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var drawingView:DrawingView?=null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        drawingView= findViewById(R.id.drawing_view)
+        drawingView?.setSizeForBrush(20.toFloat())
+       val ib_brush: ImageButton = findViewById(R.id.ib_brush_men)
+        ib_brush.setOnClickListener {
+            showBrushSizeChooserDialog()
+        }
+
+    }
+    private fun showBrushSizeChooserDialog(){
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Brush size: ")
+        val smallBtn = brushDialog.iBtn_small_brush
+        smallBtn.setOnClickListener {
+            drawingView?.setSizeForBrush(8.toFloat())
+            brushDialog.dismiss()
+        }
+        val mediumBtn = brushDialog.iBtn_medium_brush
+        mediumBtn.setOnClickListener {
+            drawingView?.setSizeForBrush(16.toFloat())
+            brushDialog.dismiss()
+        }
+        val largeBtn = brushDialog.iBtn_large_brush
+        largeBtn.setOnClickListener {
+            drawingView?.setSizeForBrush(24.toFloat())
+            brushDialog.dismiss()
+        }
+        brushDialog.show()
 
     }
 }
